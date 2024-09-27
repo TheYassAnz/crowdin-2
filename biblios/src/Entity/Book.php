278f9b831d9f877b968e065fa17b0dfce047e523
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2024-09-27 15:26:41 +0200

    feat: add relation Book > Editor

--- biblios/src/Entity/Book.php
+++ biblios/src/Entity/Book.php
@@ -35,6 +35,10 @@ class Book
     #[ORM\Column(length: 255)]
     private ?BookStatus $status = null;
 
+    #[ORM\ManyToOne(inversedBy: 'books')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Editor $editor = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -120,6 +124,18 @@ class Book
     public function setStatus(BookStatus $status): static
     {
         $this->status = $status;
+
+        return $this;
+    }
+
+    public function getEditor(): ?Editor
+    {
+        return $this->editor;
+    }
+
+    public function setEditor(?Editor $editor): static
+    {
+        $this->editor = $editor;
 
         return $this;
     }
