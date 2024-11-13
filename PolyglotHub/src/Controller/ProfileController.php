1a412d14713874a565241dc94e73fb3a6fb14505
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2024-11-13 18:31:30 +0100

    feat: profile form

--- PolyglotHub/src/Controller/ProfileController.php
+++ PolyglotHub/src/Controller/ProfileController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Profil;
+use App\Form\ProfileType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -17,8 +20,24 @@ class ProfileController extends AbstractController
             return $this->redirectToRoute('app_login');
         }
 
-        return $this->render('profile/index.html.twig', [
+        return $this->render('profile/index.html.twig', parameters: [
             'user' => $user,
         ]);
+    }
+
+    #[Route('/profile', name: 'app_profile')]
+    public function new(Request $request): Response
+    {
+        $profil = new Profil();
+        $form = $this->createForm(ProfileType::class, $profil);
+
+        return $this->render('profile/new.html.twig', [
+            'form' => $form,
+        ]);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // faire quelque chose
+        }
     }
 }
---
+++ PolyglotHub/src/Form/ProfileType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Profil;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProfileType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add(
+                'description',
+                TextareaType::class,
+                ['label' => 'Description']
+            )
+            ->add('skills')
+            ->add('languages')
+            ->add('user', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Profil::class,
+        ]);
+    }
+}
